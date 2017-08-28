22931b31322c4fc4126cf9b6346c2d4dc6169ef5
Kelvin <yeung_kelvin@hotmail.com>
2017-08-28 14:05:20 +0000

    Installed Bootstrap

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,7 @@
 class ArticlesController < ApplicationController
     
+    before_action :set_article, only: [:edit, :update, :show, :destroy]
+    
     def index
         @articles = Article.all
     end
@@ -20,22 +22,22 @@ class ArticlesController < ApplicationController
     end
     
     def show
-        @article = Article.find(params[:id])
+        
     end
     
     def destroy
-        @article = Article.find(params[:id])
+        
         @article.destroy
         flash[:notice] = "Score has been deleted"
         redirect_to articles_path
     end
     
     def edit
-        @article = Article.find(params[:id])
+        
     end
     
     def update
-        @article = Article.find(params[:id])
+        
         if @article.update(article_params)
             flash[:notice] = "Score was successfully submitted"
             redirect_to article_path(@article)
@@ -45,7 +47,12 @@ class ArticlesController < ApplicationController
     end
     
     private
-        def article_params
-            params.require(:article).permit(:score, :comment)
-        end
+    
+    def set_article
+        @article = Article.find(params[:id])
+    end
+    
+    def article_params
+        params.require(:article).permit(:score, :comment)
+    end
 end
