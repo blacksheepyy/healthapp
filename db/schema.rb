0fa118f7b5f7c36b7f551c81e4ac38608dee57de
Kelvin <yeung_kelvin@hotmail.com>
2017-08-28 21:16:03 +0000

    create user table

--- db/schema.rb
+++ db/schema.rb
@@ -11,11 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828104858) do
+ActiveRecord::Schema.define(version: 20170828211218) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "score"
     t.text     "comment"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.string   "linemanager"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
