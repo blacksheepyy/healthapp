34d23d0ef5308f1f4efc311f5e0281e34bb9afea
Kelvin <yeung_kelvin@hotmail.com>
2017-08-28 22:44:47 +0000

    Added has-secure-password

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170828212601) do
+ActiveRecord::Schema.define(version: 20170828224154) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "score"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20170828212601) do
     t.string   "linemanager"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "password_digest"
   end
 
 end
