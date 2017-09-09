2edfb1fa7d031430f09333694f45992fea9c3253
Kelvin <yeung_kelvin@hotmail.com>
2017-09-09 12:09:18 +0000

    Added emojis

---
+++ app/assets/javascripts/jQuery.js
@@ -0,0 +1,7 @@
+$(document).on('turbolinks:load', function() {
+ 
+  $('img').click(function(){
+   $('.selected').removeClass('selected'); // removes the previous selected class
+   $(this).addClass('selected'); // adds the class to the clicked image
+});
+})
