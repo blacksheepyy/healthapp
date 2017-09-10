f73015e9e525228d64126076107f2a62f4c97082
Kelvin <yeung_kelvin@hotmail.com>
2017-09-10 21:37:38 +0000

    Added animations

--- app/assets/javascripts/jQuery.js
+++ app/assets/javascripts/jQuery.js
@@ -1,7 +1,25 @@
 $(document).on('turbolinks:load', function() {
- 
+ $('img').hover(function(){
+   $('.hover').removeClass('hover'); // removes the previous selected class
+   $(this).addClass('hover'); // adds the class to the clicked image
+});
+$('img').mouseleave(function(){
+   $('.hover').removeClass('hover'); // removes the previous selected class
+   
+});
   $('img').click(function(){
    $('.selected').removeClass('selected'); // removes the previous selected class
-   $(this).addClass('selected'); // adds the class to the clicked image
+   $(this).addClass('selected')
+   .animate({
+            opacity: '1.0',
+            height: '110px',
+            width: '110px'
+        })
+        .animate({
+            opacity: '1.0',
+            height: '100px',
+            width: '100px'
+        });
+        
 });
 })
